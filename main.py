e63609cc1776d987f85fa8efa75c8ed26c5ea574
vitJR1 <kalugin.vitaliy23@gmail.com>
2023-05-02 21:01:02 +0600

    basic struct

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hi'):
+        await message.channel.send('Hello!')
+
+if __name__ == "__main__":
+    client.run('<Your Bot Token>')
